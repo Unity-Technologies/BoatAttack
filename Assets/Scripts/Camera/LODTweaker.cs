3d1acfcb86a1e6e58ed02ee4dc5c44c6bca7099a
Andre McGrail <andre@nyuknyukgames.com>
2018-03-20 08:49:57 +0000

    Switched terrain to mesh Added LODTweaker to change lod on secondary cameras

---
+++ Assets/Scripts/Camera/LODTweaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LightweightPipeline;
+
+public class LODTweaker : MonoBehaviour {
+
+	void SetMaxLOD(Camera cam)
+	{
+		if(cam == Camera.main)
+			QualitySettings.maximumLODLevel = 0;
+		else
+			QualitySettings.maximumLODLevel = 2;
+	}
+
+    void OnEnable()
+    {
+        LightweightPipeline.beginCameraRendering += SetMaxLOD;
+    }
+
+    void OnDisable()
+    {
+        LightweightPipeline.beginCameraRendering -= SetMaxLOD;
+    }
+
+}
