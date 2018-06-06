02a089d4bab5026276425d94670dccfd75966eda
Andre McGrail <andrem@unity3d.com>
2018-06-06 14:23:57 +0100

    Updated boat physics settings Rebaked lighting Updated submodules

--- Assets/Scripts/Boat/Engine.cs
+++ Assets/Scripts/Boat/Engine.cs
@@ -34,7 +34,7 @@ public class Engine : MonoBehaviour
 
         //cache boat and boat controller
         //boatController = gameObject.GetComponent<BoatController>();
-        //buoyantObject = gameObject.GetComponent<BuoyantObject>();
+        buoyantObject = gameObject.GetComponent<BuoyantObject>();
         //cache wakeGenerator
         //wakeGen = GameObject.Find ("WakeGenerator").GetComponent<CreateWake> ();
 
@@ -42,75 +42,79 @@ public class Engine : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		vel = RB.velocity;
-
+        vel = RB.velocity;
         float velMag = vel.sqrMagnitude;
-
-
         engineSound.pitch = Mathf.Max(velMag * 0.015f, 0.3f);
+        // if (buoyantObject.percentSubmerged > 0.1f)
+        // {
+        //     RB.AddForce(Vector3.up * (Mathf.Abs(Vector3.Dot(vel, transform.forward)) * velMag), ForceMode.Force);
+        // }
+        // if(throttle > 0f)
+        // {
+        // 	if(transform.TransformPoint(enginePosition).y < 0.5f)
+        // 	{
+        // 		Vector3 forward = transform.forward + transform.TransformDirection(engineDir * 0.15f);
+        // 		forward.Normalize();
+        // 		RB.AddForceAtPosition(((forward * horsePower) * throttle), transform.TransformPoint(enginePosition), ForceMode.Acceleration);
+        // 	}
+        // 	throttle -= Time.fixedDeltaTime;
+        // }
+        // else
+        // {
+        // 	throttle = 0f;
+        // }
 
-        RB.AddForce(Vector3.up * (Mathf.Abs(Vector3.Dot(vel, transform.forward)) * velMag), ForceMode.Force);
-
-		// if(throttle > 0f)
-		// {
-		// 	if(transform.TransformPoint(enginePosition).y < 0.5f)
-		// 	{
-		// 		Vector3 forward = transform.forward + transform.TransformDirection(engineDir * 0.15f);
-		// 		forward.Normalize();
-		// 		RB.AddForceAtPosition(((forward * horsePower) * throttle), transform.TransformPoint(enginePosition), ForceMode.Acceleration);
-		// 	}
-		// 	throttle -= Time.fixedDeltaTime;
-		// }
-		// else
-		// {
-		// 	throttle = 0f;
-		// }
-
-		// if(engineDir.x > 0.1f)
-		// 		engineDir.x -= Time.fixedDeltaTime;
-		// else if(engineDir.x < -0.1f)
-		// 		engineDir.x += Time.fixedDeltaTime;
-		// else
-		// 	engineDir.x = 0f;
-	}
+        // if(engineDir.x > 0.1f)
+        // 		engineDir.x -= Time.fixedDeltaTime;
+        // else if(engineDir.x < -0.1f)
+        // 		engineDir.x += Time.fixedDeltaTime;
+        // else
+        // 	engineDir.x = 0f;
+    }
 
 	public void Accel(float modifier)
 	{
-		//if(buoyantObject.percentSubmerged > 0.05f)
-		Vector3 forward = transform.forward;
-		forward.y = 0f;
-		forward.Normalize();
-		RB.AddForce(forward * modifier * horsePower, ForceMode.Acceleration);
-		RB.AddRelativeTorque(-Vector3.right * modifier, ForceMode.Acceleration);
+        if (buoyantObject.percentSubmerged > 0.1f)
+        {
+            Vector3 forward = transform.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            RB.AddForce(forward * modifier * horsePower, ForceMode.Acceleration);
+            RB.AddRelativeTorque(-Vector3.right * modifier, ForceMode.Acceleration);
+        }
 
 
-		// if(throttle < 1f)
-		// 	throttle += modifier;
-		// if(throttle > 1f)
-		// 	throttle = 1f;
-	}
+        // if(throttle < 1f)
+        // 	throttle += modifier;
+        // if(throttle > 1f)
+        // 	throttle = 1f;
+    }
 
 	public void TurnLeft(float modifier)
 	{
-		//if(buoyantObject.percentSubmerged > 0.05f)
-		RB.AddRelativeTorque (new Vector3 (0f, -torque, torque * 0.5f) * modifier * 500f, ForceMode.Acceleration);
-
-		// if(engineDir.x < 1f)
-		// 	engineDir.x += modifier;
-		// if(engineDir.x > 1f)
-		// 	engineDir.x = 1f;
-	}
+        //if(buoyantObject.percentSubmerged > 0.05f)
+        if (buoyantObject.percentSubmerged > 0.1f)
+        {
+            RB.AddRelativeTorque(new Vector3(0f, -torque, torque * 0.5f) * modifier * 500f, ForceMode.Acceleration);
+        }
+        // if(engineDir.x < 1f)
+        // 	engineDir.x += modifier;
+        // if(engineDir.x > 1f)
+        // 	engineDir.x = 1f;
+    }
 
 	public void TurnRight(float modifier)
 	{
-		//if(buoyantObject.percentSubmerged > 0.05f)
-		RB.AddRelativeTorque (new Vector3 (0f, torque, -torque * 0.5f) * modifier * 500f, ForceMode.Acceleration);
-
-		// if(engineDir.x > -1f)
-		// 	engineDir.x -= modifier;
-		// if(engineDir.x < -1f)
-		// 	engineDir.x = -1f;
-	}
+        //if(buoyantObject.percentSubmerged > 0.05f)
+        if (buoyantObject.percentSubmerged > 0.1f)
+        {
+            RB.AddRelativeTorque(new Vector3(0f, torque, -torque * 0.5f) * modifier * 500f, ForceMode.Acceleration);
+        }
+        // if(engineDir.x > -1f)
+        // 	engineDir.x -= modifier;
+        // if(engineDir.x < -1f)
+        // 	engineDir.x = -1f;
+    }
 
 	void OnDrawGizmosSelected ()
 	{
