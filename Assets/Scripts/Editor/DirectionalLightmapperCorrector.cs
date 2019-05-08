4cd77158a123ffda430c1304227465c03afe431d
André McGrail <andrem@unity3d.com>
2019-05-08 18:04:57 +0200

    Added light map corrector to avoid NaNs
    -baked all lighting data
    -updated package versions

---
+++ Assets/Scripts/Editor/DirectionalLightmapperCorrector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DirectionalLightmapperCorrector : AssetPostprocessor
+{
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        if (!(assetPath.Contains("Lightmap-") && assetPath.Contains("_comp_dir")))
+            return;
+        
+        Color[] c = texture.GetPixels();
+
+        for (int i = 0; i < c.Length; i++)
+        {
+            c[i].a = Mathf.Max(c[i].a, 0.1f);
+        }
+        texture.SetPixels(c);
+        texture.Apply(true);
+    }
+}
