206b147919071b317b70c36742dcbc0a6baed546
André McGrail <andrem@unity3d.com>
2019-06-12 15:58:26 +0200

    edits

---
+++ Assets/Scripts/Camera/Postv3Autofocus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.LWRP;
+
+public class Postv3Autofocus : MonoBehaviour
+{
+    public VolumeProfile profile;
+
+    private DepthOfField dof;
+
+    public CinemachineBrain cm;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (profile && cm)
+        {
+            profile.TryGet(out dof);
+
+            CameraState state = cm.CurrentCameraState;
+
+            if (dof)
+                dof.focusDistance.value = (state.FinalPosition - state.ReferenceLookAt).magnitude;
+        }
+    }
+}
