bc79cc9afd97ee4e8c6b3cf8146bcb051c51504b
Ayoub Gharbi <contact@ayoub-gharbi.org>
2019-08-01 09:09:51 +0100

    Menu buttons' navigation and hover effect

---
+++ Assets/Scripts/Utility/HoverComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class HoverComponent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    private TextMeshProUGUI _targetText = null;
+
+    private void Start()
+    {
+        _targetText = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Behave(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Behave(false);
+    }
+
+    public void OnSelect(BaseEventData baseData)
+    {
+        Behave(true);
+    }
+
+    public void OnDeselect(BaseEventData baseData)
+    {
+        Behave(false);
+    }
+
+    void Behave(bool scale)
+    {
+        _targetText.transform.localScale = scale ? new Vector3(1.1f, 1.1f, 1.1f) : Vector3.one;
+    }
+}
