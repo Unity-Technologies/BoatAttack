09c95eb0fbb6602bb372953669c6af929d94fff2
André McGrail <andrem@unity3d.com>
2019-08-21 16:58:30 +0200

    Random prefab updates and project settings tweaks to get the project running on all platforms

---
+++ Assets/Scripts/Utility/DebugScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugScript : MonoBehaviour
+{
+    private ParticleSystem[] parts;
+
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parts = FindObjectsOfType<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int partCountTotal = 0;
+        foreach (var ps in parts)
+        {
+            partCountTotal += ps.particleCount;
+        }
+
+        var output = $"Total particles in {parts.Length} systems:{partCountTotal:N}";
+        if (!text)
+        {
+            Debug.LogError(output);
+        }
+        else
+        {
+            text.text = output;
+        }
+    }
+}
