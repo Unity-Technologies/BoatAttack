c9574184b6c0998af3495af1d8740cc6d93064f3
Mike Geig <mikegeig@gmail.com>
2019-08-22 22:28:54 -0400

    -Added buoyancy visualization

---
+++ Assets/Unity Physics Items/BuoyancyVisualizer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Entities;
+
+public class BuoyancyVisualizer : MonoBehaviour, IConvertGameObjectToEntity
+{
+	public Entity boat;
+
+	private void OnDrawGizmos()
+	{
+		if (!Application.isPlaying)
+			return;
+
+		DynamicBuffer<VoxelHeight> heights = World.Active.EntityManager.GetBuffer<VoxelHeight>(boat);
+
+	
+
+		Gizmos.color = Color.red;
+
+
+
+		for (int i = 0; i < heights.Length; i++)
+			Gizmos.DrawSphere(heights[i].Value, .1f);
+	}
+
+	public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+	{
+		
+		if (boat == Entity.Null)
+		{
+			boat = conversionSystem.GetPrimaryEntity(transform.parent);
+		}
+	}
+}
