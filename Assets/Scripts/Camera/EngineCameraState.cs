6e36bdb362fa6e0f903aebb7a4455761c23c3577
Marc Tanenbaum <marct@unity3d.com>
2020-02-02 10:52:25 -0500

    New code for Impulse listening on collisions

---
+++ Assets/Scripts/Camera/EngineCameraState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BoatAttack;
+using UnityEngine;
+
+
+namespace Boat
+{
+    public class EngineCameraState : MonoBehaviour
+    {
+        public Engine engine;
+        public Animator anim;
+//        private int slowHash = Animator.StringToHash("Main.Slow");
+//        private int fastHash = Animator.StringToHash("Main.Fast");
+
+
+        void Update()
+        {
+            anim.SetFloat("EngineSpeed", engine.VelocityMag);
+        }
+    }
+}
