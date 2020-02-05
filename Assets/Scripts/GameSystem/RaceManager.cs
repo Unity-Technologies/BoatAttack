25551dd055eee1a6e4db476ddb6e4da6007eed27
André McGrail <andrem@unity3d.com>
2020-02-05 17:09:24 +0100

    fixes
    - fixed precision error in checkpoint shader
    - fixed tangents on clouds
    - fixed touch controls appearing on switch
    - clamped skybox sun
    - changed reflection probes to baked

--- Assets/Scripts/GameSystem/RaceManager.cs
+++ Assets/Scripts/GameSystem/RaceManager.cs
@@ -299,7 +299,9 @@ namespace BoatAttack
 
         private static IEnumerator CreatePlayerUi(int player)
         {
-            var touch = Input.touchSupported && Input.multiTouchEnabled;
+            var touch = Input.touchSupported && Input.multiTouchEnabled &&
+                        (Application.platform == RuntimePlatform.Android ||
+                         Application.platform == RuntimePlatform.IPhonePlayer);
             var uiAsset = touch ? Instance.raceUiTouchPrefab : Instance.raceUiPrefab;
             var uiLoading = uiAsset.InstantiateAsync();
             yield return uiLoading;
