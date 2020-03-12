88ff83970ddf3787cbe27627bbec7bf1fb126b03
Lucas Meijer <lucas@unity3d.com>
2020-03-12 23:12:17 +0100

    make incrmeental buildpipeline pick up addressable assets

---
+++ Assets/IncrementalPipelineSupport/AddressableFilesProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using Unity.Build.Classic;
+using UnityEngine.AddressableAssets;
+
+public class AddressableFilesProvider : AdditionalFilesProviderBase
+{
+    protected override void OnPrepareAdditionalAssetsBeforeBuild()
+    {
+        if (!Directory.Exists(Addressables.BuildPath))
+            return;
+
+        var buildPath = Addressables.BuildPath;
+        var relativeRoot = Path.GetDirectoryName(Path.GetDirectoryName(buildPath));
+        var fileToCopies = Directory.GetFiles(buildPath, "*.*", SearchOption.AllDirectories).ToArray();
+        foreach (var fileToCopy in fileToCopies)
+        {
+            var relative = fileToCopy.Substring(relativeRoot.Length+1);
+            var targetFile = $"{StreamingAssetsDirectory}/{relative}";
+            AddFile(fileToCopy, targetFile);
+        }
+    }
+}
