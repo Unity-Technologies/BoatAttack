ba4e437273a4b2010e50940b6d689c6faa831aaa
andytouch <andyt@unity3d.com>
2020-05-20 12:56:27 +0200

    Upgrade: 2019.3.13. Added Gradient Fog Render Pass

---
+++ Assets/ScriptableRenderFeatures/GradientFog/GradientFogFunction.hlsl
@@ -0,0 +1,16 @@
+﻿#ifndef GRADIENTFOGFUNCTION_INCLUDED
+      #define GRADIENTFOGFUNCTION_INCLUDED
+      
+      float4 noFog = float4(0,0,0,1);
+      
+      void FogGradient_float(float4 a, float4 b, float4 c, float t, out float4 Out) {
+          float alpha = step(t, 0.3) * lerp(0, 0.7, t*3.3);
+          alpha += step(0.3, t) * lerp(0.7, 1, (t-0.3)*1.42);
+          
+          float4 color = step(t, 0.3) * lerp(noFog, a, t*3.3);
+          color += step(0.3, t) * step(t, 0.6) * lerp(a, b, (t-0.3)*3.3);
+          color += step(0.6, t) * lerp(b,c, saturate((t-0.6)*2.5));
+          
+          Out = float4(color.r, color.g, color.b, saturate(alpha));
+      }
+      #endif
---
+++ Assets/ScriptableRenderFeatures/RenderPasses/GradientFog.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class GradientFog : ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class GradientFogSettings
+    {
+        public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
+        public float StartDistance = 0;
+        public float EndDistance = 50;
+        public Color NearColor = new Color(0, 0.2f, 0.35f, 1);
+        public Color MiddleColor = new Color(0.62f, 0.86f, 1, 1);
+        public Color FarColor = new Color(0.85f, 0.96f, 1, 1);
+    }
+    
+    
+    class GradientFogPass : ScriptableRenderPass
+    {
+        public GradientFogSettings settings;
+
+        private Material fogMaterial;
+
+        private RenderTargetIdentifier source { get; set; }
+        private RenderTargetHandle m_TempTex;
+
+        public GradientFogPass()
+        {
+            m_TempTex.Init("_TempTex");
+            fogMaterial = new Material(Shader.Find("Shader Graphs/ShaderGraph_GradientFog"));
+        }
+
+        public void Setup(RenderTargetIdentifier cameraColorTarget)
+        {
+            source = cameraColorTarget;
+            fogMaterial.SetFloat("_StartDist", settings.StartDistance);
+            fogMaterial.SetFloat("_EndDist", settings.EndDistance);
+            fogMaterial.SetColor("_NearCol", settings.NearColor);
+            fogMaterial.SetColor("_MidCol", settings.MiddleColor);
+            fogMaterial.SetColor("_FarCol", settings.FarColor);
+        }
+        
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in an performance manner.
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get("Gradient Fog");
+
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+            
+            opaqueDesc.depthBufferBits = 0;
+            
+            cmd.GetTemporaryRT(m_TempTex.id, opaqueDesc);
+            
+            
+            Blit(cmd, source, m_TempTex.Identifier(), fogMaterial);
+            Blit(cmd, m_TempTex.Identifier(), source);
+            
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        /// Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(m_TempTex.id);
+        }
+    }
+
+    GradientFogPass m_GradientFogPass;
+    public GradientFogSettings settings = new GradientFogSettings();
+
+    public override void Create()
+    {
+        m_GradientFogPass = new GradientFogPass();
+        m_GradientFogPass.settings = settings;
+
+        // Configures where the render pass should be injected.
+        m_GradientFogPass.renderPassEvent = settings.Event;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        var src = renderer.cameraColorTarget;
+        m_GradientFogPass.Setup(src);
+        renderer.EnqueuePass(m_GradientFogPass);
+    }
+}
