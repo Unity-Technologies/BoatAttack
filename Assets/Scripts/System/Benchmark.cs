3eabf99cb02c653fec58d13d48e05957c6fa6a3c
André McGrail <andrem@unity3d.com>
2020-08-13 15:48:00 +0200

    more work, cleanup of UI prefabs

--- Assets/Scripts/System/Benchmark.cs
+++ Assets/Scripts/System/Benchmark.cs
@@ -1,64 +1,98 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
 using Object = UnityEngine.Object;
 #if UNITY_EDITOR
-using System.Collections;
 using UnityEditor;
 using UnityEditor.SceneManagement;
-
 #endif
 
 namespace BoatAttack
 {
     public class Benchmark : MonoBehaviour
     {
-        public Object[] scenes;
+        public List<BenchmarkSettings> settings = new List<BenchmarkSettings>();
+        private static BenchmarkSettings _settings;
 
-        private static bool _exitOnCompletion;
-        public string scene;
+        //public AssetReference perfStatsUI;
+        //public AssetReference perfSummaryUI;
 
         private void Start()
         {
-#if UNITY_EDITOR
-            
-#endif
+            SceneManager.sceneLoaded += OnSceneLoaded;
             DontDestroyOnLoad(gameObject);
-            LoadBenchmark(scene, 4, true, true);
+            LoadBenchmark(settings[0]);
         }
 
-        public static void LoadBenchmark(string scene, int runs, bool perfstats, bool quitOnFinish)
+        private void LoadBenchmark(BenchmarkSettings setting)
         {
-            _exitOnCompletion = quitOnFinish;
-            AppSettings.LoadScene(scene);
+            settings.RemoveAt(0);
+            _settings = setting;
+            AppSettings.LoadScene(setting.scene);
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == _settings.scene)
+            {
+                switch (_settings.type)
+                {
+                    case BenchmarkType.Track:
+                        SetupFlyThroughBenchmark();
+                        break;
+                    case BenchmarkType.Static:
+                        SetupStaticBenchmark();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private void SetupFlyThroughBenchmark()
+        {
+            var go = GameObject.FindGameObjectWithTag("benchmark_flythrough");
+
+        }
+
+        private void SetupStaticBenchmark()
+        {
+            var go = GameObject.FindGameObjectWithTag("benchmark_static");
         }
 
         public static void EndBenchmark()
         {
-            if (_exitOnCompletion)
+            if (_settings.exitOnCompletion)
             {
                 AppSettings.ExitGame();
             }
         }
     }
-    
+
 #if UNITY_EDITOR
     [InitializeOnLoad]
+#endif
     public class BenchmarkTool
     {
         static BenchmarkTool()
         {
             EditorApplication.playModeStateChanged += Cleanup;
         }
-        
+
+#if UNITY_EDITOR
         [MenuItem("Boat Attack/Benchmark/Island Flythrough")]
         public static void IslandFlyThrough()
         {
             EditorApplication.EnterPlaymode();
-            //EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
-            var go = new GameObject("BenchmarkManager");
-            var bench = go.AddComponent<Benchmark>();
-            bench.scene = "benchmark_island-flythrough";
+            var settings = new BenchmarkSettings("benchmark_island-flythrough",
+                4,
+                true,
+                true,
+                true,
+                BenchmarkType.Track);
+            CreateBenchmark(settings);
         }
 
         private static void Cleanup(PlayModeStateChange state)
@@ -71,8 +105,14 @@ namespace BoatAttack
                     Object.DestroyImmediate(go);
             }
         }
-    }
 #endif
+        public static void CreateBenchmark(BenchmarkSettings settings)
+        {
+            var go = new GameObject("BenchmarkManager");
+            var bench = go.AddComponent<Benchmark>();
+            bench.settings.Add(settings);
+        }
+    }
 
     [Serializable]
     public class PerfBasic
@@ -122,6 +162,34 @@ namespace BoatAttack
             Os = SystemInfo.operatingSystem.Replace(" ", "");
             Quality = QualitySettings.names[QualitySettings.GetQualityLevel()].Replace(" ", "");
             Resolution = $"{Display.main.renderingWidth}x{Display.main.renderingHeight}";
+        }
+    }
+
+    [Serializable]
+    public enum BenchmarkType
+    {
+        Track,
+        Static
+    }
+
+    [Serializable]
+    public class BenchmarkSettings
+    {
+        public string scene = "benchmark_island-flythrough";
+        public int runs = 4;
+        public bool exitOnCompletion = true;
+        public bool warmup = true;
+        public bool stats = false;
+        public BenchmarkType type;
+
+        public BenchmarkSettings(string scene, int runs, bool exitOnCompletion, bool warmup, bool stats, BenchmarkType type)
+        {
+            this.scene = scene;
+            this.runs = runs;
+            this.exitOnCompletion = exitOnCompletion;
+            this.warmup = warmup;
+            this.stats = stats;
+            this.type = type;
         }
     }
 }
