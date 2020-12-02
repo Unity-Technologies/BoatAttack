116bbb4ea2e5eb26e27e8d1ce4ff27ea37e51b45
André McGrail <andrem@unity3d.com>
2020-12-02 13:43:05 +0100

    cloud mesh generator moved to utility, and asset

--- Assets/Scripts/Environment/CloudRenderer.cs
+++ Assets/Scripts/Environment/CloudRenderer.cs
@@ -5,60 +5,27 @@ using System.Linq;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
-using Unity.Collections;
+using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
+[ExecuteAlways]
 public class CloudRenderer : MonoBehaviour
 {
-    public Sprite[] sprites;
-    private Mesh[] cloudMeshes;
+    public Mesh[] cloudMeshes;
     public Material cloudMaterial;
     public ParticleSystem ps;
 
     private NativeArray<ParticleSystem.Particle> particles;
     private ParticleSystem.Particle[] parts;
 
-    private void Start()
-    {
-        cloudMeshes = new Mesh[sprites.Length];
-        int i = 0;
-        foreach (var sprite in sprites)
-        {
-            // verts
-            var verts = new List<Vector3>();
-            sprite.vertices.ToList().ForEach(x => verts.Add(new Vector3(x.x, x.y, 0.0f)));
-            // triangles
-            var tris = new List<int>();
-            sprite.triangles.ToList().ForEach(x => tris.Add(x));
-            // normals
-            var normals = new Vector3[verts.Count];
-            for (var index = 0; index < normals.Length; index++)
-            {
-                normals[index] = Vector3.forward;
-            }
-
-            var mesh = new Mesh
-            {
-                name = sprite.name,
-                vertices = verts.ToArray(),
-                triangles = tris.ToArray(),
-                normals = normals.ToArray(),
-                uv = sprite.uv,
-                bounds = new Bounds(Vector3.zero, Vector3.one * 1000f)
-            };
-
-            cloudMeshes[i] = mesh;
-            i++;
-        }
-    }
-
     private void OnEnable()
     {
         RenderPipelineManager.beginCameraRendering += RenderPipelineManagerOnbeginCameraRendering;
         RenderPipelineManager.beginFrameRendering += RenderPipelineManagerOnbeginFrameRendering;
 
-        particles = new NativeArray<ParticleSystem.Particle>(ps.main.maxParticles, Allocator.Persistent);
-        parts = new ParticleSystem.Particle[ps.main.maxParticles];
+        var main = ps.main;
+        particles = new NativeArray<ParticleSystem.Particle>(main.maxParticles, Allocator.Persistent);
+        parts = new ParticleSystem.Particle[main.maxParticles];
     }
 
     private void OnDisable()
@@ -71,15 +38,18 @@ public class CloudRenderer : MonoBehaviour
 
     private void RenderPipelineManagerOnbeginFrameRendering(ScriptableRenderContext arg1, Camera[] arg2)
     {
-        var camPos = Camera.main.transform.position;
-        ps.GetParticles(parts);
-        var l = parts.ToList().OrderByDescending(x => Vector3.Distance(x.position * 64f, camPos));
-        parts = l.ToArray();
+        if (Camera.main != null)
+        {
+            var camPos = Camera.main.transform.position;
+            ps.GetParticles(parts);
+            var l = parts.ToList().OrderByDescending(x => Vector3.Distance(x.position * 64f, camPos));
+            parts = l.ToArray();
+        }
     }
 
     private void RenderPipelineManagerOnbeginCameraRendering(ScriptableRenderContext context, Camera camera)
     {
-        if (!cloudMaterial) return;
+        if (!cloudMaterial || cloudMeshes == null || cloudMeshes.Length == 0) return;
 
         var camPos = camera.transform.position * (1f / 64f);
 
@@ -87,7 +57,19 @@ public class CloudRenderer : MonoBehaviour
         {
             var q = Quaternion.LookRotation(particle.position - camPos);
             Random.InitState((int)particle.randomSeed);
-            Graphics.DrawMesh(cloudMeshes[Random.Range(0, cloudMeshes.Length)], Matrix4x4.TRS(particle.position, q, particle.startSize3D), cloudMaterial, LayerMask.NameToLayer("3DSkybox"), camera);
+            var mesh = cloudMeshes[Random.Range(0, cloudMeshes.Length)];
+            /*var m = Instantiate(mesh);
+            var colors = new Color[mesh.vertexCount];
+            var col = particle.GetCurrentColor(ps);
+            for (var index = 0; index < colors.Length; index++)
+            {
+                colors[index] = col;
+            }
+
+            m.SetColors(colors);
+            m.UploadMeshData(true);*/
+
+            Graphics.DrawMesh(mesh, Matrix4x4.TRS(particle.position, q, particle.startSize3D), cloudMaterial, LayerMask.NameToLayer("3DSkybox"), camera);
         }
     }
 }
