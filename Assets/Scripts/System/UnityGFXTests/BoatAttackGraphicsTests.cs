56bd813f72a26feba41db331a45f1e6374fd6a50
Andrem <andrem@unity3d.com>
2021-07-06 11:08:53 +0200

    Init Commit

---
+++ Assets/Scripts/System/UnityGFXTests/BoatAttackGraphicsTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.TestTools.Graphics;
+using UnityEngine.SceneManagement;
+
+public class BoatAttackGraphicsTests
+{
+    
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, "STATIC_EVERYTHING");
+        return null;
+    }
+    
+    
+    [UnityTest, Category("BoatAttack")]
+    [PrebuildSetup("SetupGraphicsTestCases")]
+    [UseGraphicsTestCases]
+    public IEnumerator Run(GraphicsTestCase testCase)
+    {
+        SceneManager.LoadScene(testCase.ScenePath);
+
+        // Always wait one frame for scene load
+        yield return null;
+
+        var camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        var settings = Object.FindObjectOfType<BoatAttackGraphicsTestsSettings>();
+        if (!settings)
+            settings = camera.gameObject.AddComponent<BoatAttackGraphicsTestsSettings>();
+
+        settings.ImageComparisonSettings.UseHDR = true;
+        settings.ImageComparisonSettings.UseBackBuffer = true;
+        settings.ImageComparisonSettings.ActiveImageTests = ImageComparisonSettings.ImageTests.IncorrectPixelsCount;
+        settings.WaitFrames = 5;
+
+        settings.ImageComparisonSettings.TargetWidth = 1280;
+        settings.ImageComparisonSettings.TargetWidth = 720;
+
+#if STATIC_EVERYTHING
+        Debug.Log("hello, this worked");
+#endif
+        
+        for (int i = 0; i < settings.WaitFrames; i++)
+            yield return new WaitForEndOfFrame();
+
+        ImageAssert.AreEqual(testCase.ReferenceImage, camera, settings.ImageComparisonSettings);
+    }
+    
+
+#if UNITY_EDITOR
+    [TearDown]
+    public void DumpImagesInEditor()
+    {
+        UnityEditor.TestTools.Graphics.ResultsUtility.ExtractImagesFromTestProperties(TestContext.CurrentContext.Test);
+    }
+#endif
+    
+/*    [TearDown]
+    public void TearDown()
+    {
+        
+    }*/
+}
---
+++ Assets/Scripts/System/UnityGFXTests/BoatAttackGraphicsTestsSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.TestTools.Graphics;
+
+public class BoatAttackGraphicsTestsSettings : GraphicsTestSettings
+{
+    public int WaitFrames = 0;
+
+    public BoatAttackGraphicsTestsSettings()
+    {
+        ImageComparisonSettings.TargetWidth = 512;
+        ImageComparisonSettings.TargetHeight = 512;
+        ImageComparisonSettings.AverageCorrectnessThreshold = 0.005f;
+        ImageComparisonSettings.PerPixelCorrectnessThreshold = 0.005f;
+    }
+}
