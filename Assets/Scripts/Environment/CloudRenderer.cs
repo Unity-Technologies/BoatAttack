dfaa9ef49a727c54c58ac1da0c5375760b0b6e8a
Andrem <andrem@unity3d.com>
2021-11-26 13:17:49 +0100

    Fix cloud GC collection

--- Assets/Scripts/Environment/CloudRenderer.cs
+++ Assets/Scripts/Environment/CloudRenderer.cs
@@ -13,7 +13,8 @@ public class CloudRenderer : MonoBehaviour
     public ParticleSystem ps;
 
     private NativeArray<ParticleSystem.Particle> particles;
-    private ParticleSystem.Particle[] parts;
+    //private ParticleSystem.Particle[] parts;
+    private MaterialPropertyBlock[] mpbs;
     
     private void OnEnable()
     {
@@ -22,7 +23,12 @@ public class CloudRenderer : MonoBehaviour
         
         var main = ps.main;
         particles = new NativeArray<ParticleSystem.Particle>(main.maxParticles, Allocator.Persistent);
-        parts = new ParticleSystem.Particle[main.maxParticles];
+        //parts = new ParticleSystem.Particle[main.maxParticles];
+        mpbs = new MaterialPropertyBlock[main.maxParticles];
+        for (var index = 0; index < mpbs.Length; index++)
+        {
+            mpbs[index] = new MaterialPropertyBlock();
+        }
     }
 
     private void OnDisable()
@@ -43,13 +49,14 @@ public class CloudRenderer : MonoBehaviour
         if (!cloudMaterial || cloudMeshes == null || cloudMeshes.Length == 0) return;
         
         var camPos = camera.transform.position;
-        var aliveCount = ps.GetParticles(parts);
-        var l = parts.ToList().OrderByDescending(x => Vector3.Distance(x.position, camPos));
-        parts = l.ToArray();
+        var aliveCount = ps.GetParticles(particles);
+        particles.OrderByDescending(x => Vector3.Distance(x.position, camPos));
+        
+        var scale = Vector3.zero;
 
         for (var index = 0; index < aliveCount; index++)
         {
-            var particle = parts[index];
+            var particle = particles[index];
             if (particle.remainingLifetime <= 0.001f) continue;
 
             var pos = particle.position * SkyboxSystem.SkyboxScale + camPos * (1 - SkyboxSystem.SkyboxScale);
@@ -58,14 +65,13 @@ public class CloudRenderer : MonoBehaviour
             Random.InitState((int)particle.randomSeed);
             var mesh = cloudMeshes[Random.Range(0, cloudMeshes.Length)];
 
-            var scale = particle.startSize3D;
+            scale = particle.startSize3D;
             scale.x *= Random.value > 0.5f ? 1f : -1f;
             
-            MaterialPropertyBlock mpb = new MaterialPropertyBlock();
-            mpb.SetVector("_BA_CloudData", new Vector4(scale.x, 0f, 0f, particle.GetCurrentColor(ps).a / 255f));
+            mpbs[index].SetVector("_BA_CloudData", new Vector4(scale.x, 0f, 0f, particle.GetCurrentColor(ps).a / 255f));
             
             Graphics.DrawMesh(mesh, Matrix4x4.TRS(pos, q, scale * SkyboxSystem.SkyboxScale),
-                cloudMaterial, LayerMask.NameToLayer("3DSkybox"), camera, 0, mpb, false, false, false);
+                cloudMaterial, LayerMask.NameToLayer("3DSkybox"), camera, 0, mpbs[index], false, false, false);
         }
     }
 }
