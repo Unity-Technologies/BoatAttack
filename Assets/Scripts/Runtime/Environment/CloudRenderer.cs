94b08b4ad33542722f2393de951a10992c7dbb7f
Andre McGrail <andrem@unity3d.com>
2023-10-31 18:01:24 +0100

    small tweaks

--- Assets/Scripts/Runtime/Environment/CloudRenderer.cs
+++ Assets/Scripts/Runtime/Environment/CloudRenderer.cs
@@ -1,6 +1,5 @@
-using System.Linq;
-using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.Rendering;
 using WaterSystem.Rendering;
 
@@ -11,17 +10,19 @@ public class CloudRenderer : MonoBehaviour
     public Material cloudMaterial;
     public ParticleSystem ps;
 
-    private NativeArray<ParticleSystem.Particle> particles;
-    //private ParticleSystem.Particle[] parts;
+    private ParticleSystem.Particle[] particles;
     private MaterialPropertyBlock[] mpbs;
+    private Vector3 cameraPosition;
     
+    private static readonly int BaCloudData = Shader.PropertyToID("_BA_CloudData");
+
     private void OnEnable()
     {
         RenderPipelineManager.beginCameraRendering += RenderPipelineManagerOnbeginCameraRendering;
         PlanarReflections.BeginPlanarReflections += RenderPipelineManagerOnbeginCameraRendering;
         
         var main = ps.main;
-        particles = new NativeArray<ParticleSystem.Particle>(main.maxParticles, Allocator.Persistent);
+        particles = new ParticleSystem.Particle[main.maxParticles];
         mpbs = new MaterialPropertyBlock[main.maxParticles];
         for (var index = 0; index < mpbs.Length; index++)
         {
@@ -33,8 +34,6 @@ public class CloudRenderer : MonoBehaviour
     {
         RenderPipelineManager.beginCameraRendering -= RenderPipelineManagerOnbeginCameraRendering;
         PlanarReflections.BeginPlanarReflections -= RenderPipelineManagerOnbeginCameraRendering;
-
-        particles.Dispose();
     }
 
     private void RenderPipelineManagerOnbeginCameraRendering(ScriptableRenderContext context, Camera camera)
@@ -46,34 +45,44 @@ public class CloudRenderer : MonoBehaviour
     {
         if (!cloudMaterial || cloudMeshes == null || cloudMeshes.Length == 0) return;
         
-        var camPos = camera.transform.position;
         var aliveCount = ps.GetParticles(particles);
-        particles.OrderByDescending(x => Vector3.Distance(x.position, camPos));
         
-        var scale = Vector3.zero;
-        var data = Vector4.zero;
+        Profiler.BeginSample("Sort Clouds");
+        cameraPosition = camera.transform.position;
+        // order the array of particles by distance to camera
+        System.Array.Sort(particles, DistanceSort);
+        Profiler.EndSample();
 
+        Profiler.BeginSample("Draw Clouds");
         for (var index = 0; index < aliveCount; index++)
         {
             var particle = particles[index];
             if (particle.remainingLifetime <= 0.001f) continue;
 
-            var pos = particle.position * SkyboxSystem.SkyboxScale + camPos * (1 - SkyboxSystem.SkyboxScale);
+            var pos = particle.position * SkyboxSystem.SkyboxScale + camera.transform.position * (1 - SkyboxSystem.SkyboxScale);
 
-            var q = Quaternion.LookRotation(particle.position - camPos);
+            var q = Quaternion.LookRotation(particle.position - camera.transform.position);
             Random.InitState((int)particle.randomSeed);
             var mesh = cloudMeshes[Random.Range(0, cloudMeshes.Length)];
 
-            scale = particle.startSize3D;
+            var scale = particle.startSize3D;
             scale.x *= Random.value > 0.5f ? 1f : -1f;
 
+            var data = Vector4.zero;
             data.x = scale.x;
             data.w = particle.GetCurrentColor(ps).a / 255f;
             
-            mpbs[index].SetVector("_BA_CloudData", data);
+            mpbs[index].SetVector(BaCloudData, data);
             
             Graphics.DrawMesh(mesh, Matrix4x4.TRS(pos, q, scale * SkyboxSystem.SkyboxScale),
                 cloudMaterial, LayerMask.NameToLayer("3DSkybox"), camera, 0, mpbs[index], false, false, false);
         }
+        Profiler.EndSample();
+    }
+
+    private int DistanceSort(ParticleSystem.Particle a, ParticleSystem.Particle b)
+    {
+        return Vector3.Distance(a.position, cameraPosition)
+            .CompareTo(Vector3.Distance(b.position, cameraPosition));
     }
 }
