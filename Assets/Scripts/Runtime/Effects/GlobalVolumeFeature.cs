bff06dcbca5a4e9093a6a1977e2d98d76f11f677
Alexander Degtyarev <alexander.degtyarev@unity3d.com>
2024-02-02 13:04:58 +0100

    RG module is no longer in experimental

--- Assets/Scripts/Runtime/Effects/GlobalVolumeFeature.cs
+++ Assets/Scripts/Runtime/Effects/GlobalVolumeFeature.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.RenderGraphModule;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.RenderGraphModule;
 
 //[DisallowMultipleRendererFeature] // once not internal, this needs to be here
 public class GlobalVolumeFeature : ScriptableRendererFeature
--- Assets/Scripts/Runtime/Effects/KawaseBlur.cs
+++ Assets/Scripts/Runtime/Effects/KawaseBlur.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.RenderGraphModule;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
