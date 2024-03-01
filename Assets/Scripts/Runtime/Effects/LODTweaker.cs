b093a53da472250702deaa855468a1f1e07f4154
Alexander Degtyarev <alexander.degtyarev@unity3d.com>
2024-03-01 17:59:39 +0100

    mv lod tweaker to effects as it's implemented as renderfeature

---
+++ Assets/Scripts/Runtime/Effects/LODTweaker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace BoatAttack
+{
+    public class LodTweaker : ScriptableRendererFeature
+    {
+        [SerializeField] private float reflectionCameraLodBias = 3;
+        [SerializeField] private float nonreflectionCameraLodBias = 0.5f;
+        void SetMaxLod(Camera cam) {
+            if (cam == Camera.main || cam.cameraType == CameraType.SceneView || cam.cameraType == CameraType.Reflection)
+                QualitySettings.lodBias = reflectionCameraLodBias;
+            else
+                QualitySettings.lodBias = nonreflectionCameraLodBias;
+        }
+        public override void Create()
+        {
+            //no pass;
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            SetMaxLod(renderingData.cameraData.camera);
+        }
+        
+    }
+}
