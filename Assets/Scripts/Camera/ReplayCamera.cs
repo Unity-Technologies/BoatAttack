27755495f6a24e995a27bb7b15694a6f32467d76
jimma-unity <jamespa@unity3d.com>
2024-12-12 11:56:43 +0100

    Set enabled to false when spectator mode disabled.

--- Assets/Scripts/Camera/ReplayCamera.cs
+++ Assets/Scripts/Camera/ReplayCamera.cs
@@ -47,12 +47,11 @@ namespace BoatAttack
         {
             _spectatorEnabled = true;
             SetRandomTarget();
-            //droneCamera.Priority = 100;
         }
 
         public void DisableSpectatorMode()
         {
-            //droneCamera.Priority = -100;
+            _spectatorEnabled = false;
         }
 
         void SetRandomTarget() => SetTarget(Random.Range(0, RaceManager.RaceData.boatCount));
